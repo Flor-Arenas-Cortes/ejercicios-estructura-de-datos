4729759fd0ff25fdc05ae610b31b31e89611f395
Flor Arenas <flarenas@correo.univo.edu.mx>
2025-05-31 18:39:45 -0600

    Se agreo el ejercicio de Arreglo (cajones con carpetas)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.LinkedList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Arreglo (cajones con carpetas)
+        String [] carpetas = {"Matematicas", "Español", "Ciencias", "Fisica"};
+        for (int i = 0; i < carpetas.length; i++){
+            System.out.println("Cajón " + i + ": " + carpetas[i]);
+        }
     }
 }
