e1f52cfab4ab346156c9b3822ca665dd564a85e3
Flor Arenas <flarenas@correo.univo.edu.mx>
2025-05-31 18:43:35 -0600

    Se agrego el ejerccio de Lista enlazada

--- src/App.java
+++ src/App.java
@@ -4,8 +4,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         //Arreglo (cajones con carpetas)
         String [] carpetas = {"Matematicas", "Español", "Ciencias", "Fisica"};
+        System.out.println("====== Arreglo sencillo ======");
         for (int i = 0; i < carpetas.length; i++){
             System.out.println("Cajón " + i + ": " + carpetas[i]);
+        }
+
+        //Lista enlazada (cadena de correos)
+        LinkedList<String> correos = new LinkedList<>();
+        correos.add("correo_ejemplo1@correo.com");
+        correos.add("correo_ejemplo2@correo.com");
+        correos.add("correo_ejemplo3@correo.com");
+        correos.add("correo_ejemplo4@correo.com");
+
+        System.out.println("====== Lista enlazada ======");
+        for (String correo : correos){
+            System.out.println("Reenviado a: " + correo);
         }
     }
 }
